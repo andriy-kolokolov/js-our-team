bf87fe3e7f641d364d48c279cd953b331759f8e4
Andriy-Kolokolov <koloandre22@gmail.com>
2023-04-28 18:37:58 +0200

    code improvements

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,7 @@ const profile3 = {name: "Walter Gordon", role: "Office Manager", imgPath: "asset
 const profile4 = {name: "Angela Lopez", role: "Social Media Manager", imgPath: "assets/img/angela-lopez-social-media-manager.jpg"};
 const profile5 = {name: "Scott Estrada", role: "Developer", imgPath: "assets/img/scott-estrada-developer.jpg"};
 const profile6 = {name: "Barbara Ramos", role: "Graphic Designer", imgPath: "assets/img/barbara-ramos-graphic-designer.jpg"};
-// add objects to db (list)
+// add objects to list
 listProfiles = [profile1, profile2, profile3, profile4, profile5, profile6];
 
 // print in log objects info 
@@ -17,48 +17,53 @@ listProfiles.forEach(
     profile => console.log(profile.name + " " + profile.role + " " + profile.imgPath)
 );
 
-
-const eleFadeInTime = 10; // elements fade in timing
-const eleInitialOpacity = "0";  // elements initial opacity
-
 loadProfiles(eleListProfileNames, eleListProfileRoles, eleListProfileImages);
 
+console.log("xui test")
 function loadProfiles(eleListProfileNames, eleListProfileRoles, eleListProfileImages) {
-   const timeoutInterval = 350;
+   const showProfilesDelay = 350;  // delay between fade-in and show each profile
+   const eleFadeInTime = 10; // elements fade in timing
+   const eleInitialOpacity = "0";  // elements initial opacity
+   const eleEndOpacity = "0.65"  //ele end opacity
+   
    // load and show profile name
    eleListProfileNames.forEach((nameElement, index) => {
       nameElement.innerHTML = `${listProfiles[index].name}`;
       nameElement.style.opacity = eleInitialOpacity;
       setTimeout(() => {
-         unFade(nameElement, eleFadeInTime);
-      }, index * timeoutInterval);
+         unFade(nameElement, eleFadeInTime, eleEndOpacity);
+      }, index * showProfilesDelay);
    });
    // load and show profile role
    eleListProfileRoles.forEach((roleElement, index) => {
       roleElement.innerHTML = `${listProfiles[index].role}`;
       roleElement.style.opacity = eleInitialOpacity;
       setTimeout(() => {
-         unFade(roleElement, eleFadeInTime);
-      }, index * timeoutInterval);
+         unFade(roleElement, eleFadeInTime, eleEndOpacity);
+      }, index * showProfilesDelay);
    });
    // load and show profile image
    eleListProfileImages.forEach((imgElement, index) => {
       imgElement.src = `${listProfiles[index].imgPath}`;
       imgElement.style.opacity = eleInitialOpacity;
       setTimeout(() => {
-         unFade(imgElement, eleFadeInTime);
-      }, index * timeoutInterval);
+         unFade(imgElement, eleFadeInTime, 1);
+      }, index * showProfilesDelay);
    });
 }
 
-function unFade(element, fadeInTime) {
+function unFade(element, fadeInTime, endOpacity) {
    let op = 0.1;  // initial opacity
    let timer = setInterval(function () {
-      if (op >= 1) {
+      if (op >= endOpacity) {
          clearInterval(timer);
       }
       element.style.opacity = op;
       element.style.filter = 'alpha(opacity=' + op * 100 + ")";
       op += op * 0.1;
    }, fadeInTime);
+}
+
+function showTypingAnimation() {
+   
 }
